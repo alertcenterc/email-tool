cd4c4711fd585044349d944e178fbce52abda96b
Simplicode <simplicode01@gmail.com>
2026-06-21 19:44:11 +0100

    backend development update

--- email-tool-frontend/src/pages/others/SelectWithdrawalMethod.jsx
+++ email-tool-frontend/src/pages/others/SelectWithdrawalMethod.jsx
@@ -7,6 +7,7 @@ import {
   Container,
   Stack,
   Typography,
+  Paper
 } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 
@@ -71,90 +72,33 @@ export default function SelectWithdrawalMethod() {
   };
 
   return (
-    <Container maxWidth="md">
-      <Box py={6}>
-        <Box textAlign="center" mb={2}>
+    <Box
+      sx={{
+        minHeight: "100dvh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        px: 2,
+        py: 3,
+        bgcolor: "#f5f5f5",
+      }}
+    >
+      <Paper
+        elevation={4}
+        sx={{
+          width: "100%",
+          maxWidth: 420,
+          p: 3,
+          borderRadius: 4,
+        }}
+      >
           <Typography variant="h6" color="info" fontWeight="bold">
             Choose Withdrawal Method
           </Typography>
-        </Box>
 
-        {/* Paypal */}
-        <Stack spacing={2}>
-          <Card variant="outlined" sx={{ overflow: "hidden" }}>
-            <CardContent>
-              <Stack
-                direction={{ xs: "column", sm: "row" }}
-                spacing={2}
-                alignItems={{ xs: "flex-start", sm: "center" }}
-                justifyContent="space-between"
-              >
-                <Stack direction="row" spacing={2} alignItems="center" flex={1}>
-                  <FaPaypal size={24} color="blue" />
-                  <Box>
-                    <Typography fontWeight="bold">PayPal</Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      Receive your earnings to your PayPal account
-                    </Typography>
-                  </Box>
-                </Stack>
-
-                <CardActions sx={{ p: 0, mt: { xs: 2, sm: 0 } }}>
-                  <Button
-                    onClick={onSubmitPaypal
-                    }
-                    variant="contained"
-                    size="small"
-                  >
-                    Select
-                  </Button>
-                </CardActions>
-              </Stack>
-            </CardContent>
-          </Card>
-        </Stack>
-
-        {/* apple pay */}
-        <Stack spacing={2}>
-          <Card variant="outlined" sx={{ overflow: "hidden" }}>
-            <CardContent>
-              <Stack
-                direction={{ xs: "column", sm: "row" }}
-                spacing={2}
-                alignItems={{ xs: "flex-start", sm: "center" }}
-                justifyContent="space-between"
-              >
-                <Stack direction="row" spacing={2} alignItems="center" flex={1}>
-                  <FaApplePay size={36} />
-                  <Box>
-                    <Typography fontWeight="bold">Apple Pay</Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      Withdraw directly to your Apple Pay easily
-                    </Typography>
-                  </Box>
-                </Stack>
-
-                <CardActions sx={{ p: 0, mt: { xs: 2, sm: 0 } }}>
-                  <Button
-                    onClick={onSubmitApplepay}
-                    variant="contained"
-                    size="small"
-                  >
-                    Select
-                  </Button>
-                </CardActions>
-              </Stack>
-            </CardContent>
-          </Card>
-        </Stack>
-
-        <Stack spacing={2}>
-          {methods.map((method) => (
-            <Card
-              key={method.id}
-              variant="outlined"
-              sx={{ overflow: "hidden" }}
-            >
+          {/* Paypal */}
+          <Stack spacing={2}>
+            <Card variant="outlined" sx={{ overflow: "hidden" }}>
               <CardContent>
                 <Stack
                   direction={{ xs: "column", sm: "row" }}
@@ -168,26 +112,18 @@ export default function SelectWithdrawalMethod() {
                     alignItems="center"
                     flex={1}
                   >
-                    {method.icon}
-
+                    <FaPaypal size={24} color="blue" />
                     <Box>
-                      <Typography fontWeight="bold">{method.name}</Typography>
+                      <Typography fontWeight="bold">PayPal</Typography>
                       <Typography variant="body2" color="text.secondary">
-                        {method.subtitle}
+                        Receive your earnings to your PayPal account
                       </Typography>
                     </Box>
                   </Stack>
 
                   <CardActions sx={{ p: 0, mt: { xs: 2, sm: 0 } }}>
                     <Button
-                      onClick={() =>
-                        onSubmit(
-                          method.name,
-                          method.activateWalletAddress,
-                          method.walletLabel,
-                          method.walletplaceholder,
-                        )
-                      }
+                      onClick={onSubmitPaypal}
                       variant="contained"
                       size="small"
                     >
@@ -197,9 +133,99 @@ export default function SelectWithdrawalMethod() {
                 </Stack>
               </CardContent>
             </Card>
-          ))}
-        </Stack>
-      </Box>
-    </Container>
+          </Stack>
+
+          {/* apple pay */}
+          <Stack spacing={2}>
+            <Card variant="outlined" sx={{ overflow: "hidden" }}>
+              <CardContent>
+                <Stack
+                  direction={{ xs: "column", sm: "row" }}
+                  spacing={2}
+                  alignItems={{ xs: "flex-start", sm: "center" }}
+                  justifyContent="space-between"
+                >
+                  <Stack
+                    direction="row"
+                    spacing={2}
+                    alignItems="center"
+                    flex={1}
+                  >
+                    <FaApplePay size={36} />
+                    <Box>
+                      <Typography fontWeight="bold">Apple Pay</Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Withdraw directly to your Apple Pay easily
+                      </Typography>
+                    </Box>
+                  </Stack>
+
+                  <CardActions sx={{ p: 0, mt: { xs: 2, sm: 0 } }}>
+                    <Button
+                      onClick={onSubmitApplepay}
+                      variant="contained"
+                      size="small"
+                    >
+                      Select
+                    </Button>
+                  </CardActions>
+                </Stack>
+              </CardContent>
+            </Card>
+          </Stack>
+
+          <Stack spacing={2}>
+            {methods.map((method) => (
+              <Card
+                key={method.id}
+                variant="outlined"
+                sx={{ overflow: "hidden" }}
+              >
+                <CardContent>
+                  <Stack
+                    direction={{ xs: "column", sm: "row" }}
+                    spacing={2}
+                    alignItems={{ xs: "flex-start", sm: "center" }}
+                    justifyContent="space-between"
+                  >
+                    <Stack
+                      direction="row"
+                      spacing={2}
+                      alignItems="center"
+                      flex={1}
+                    >
+                      {method.icon}
+
+                      <Box>
+                        <Typography fontWeight="bold">{method.name}</Typography>
+                        <Typography variant="body2" color="text.secondary">
+                          {method.subtitle}
+                        </Typography>
+                      </Box>
+                    </Stack>
+
+                    <CardActions sx={{ p: 0, mt: { xs: 2, sm: 0 } }}>
+                      <Button
+                        onClick={() =>
+                          onSubmit(
+                            method.name,
+                            method.activateWalletAddress,
+                            method.walletLabel,
+                            method.walletplaceholder,
+                          )
+                        }
+                        variant="contained"
+                        size="small"
+                      >
+                        Select
+                      </Button>
+                    </CardActions>
+                  </Stack>
+                </CardContent>
+              </Card>
+            ))}
+          </Stack>
+      </Paper>
+    </Box>
   );
 }
