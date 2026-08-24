e57de6e40f920db1408bb8eb0b641a50ac0bd880
Simplicode <simplicode01@gmail.com>
2026-08-24 12:42:07 +0100

    backend development update

---
+++ govtsupport/src/pages/MenuCount.jsx
@@ -0,0 +1,118 @@
+import { useState } from "react";
+
+import {
+  Box,
+  Button,
+  Container,
+  Stack,
+  Typography,
+  TextField,
+  
+} from "@mui/material";
+import { Controller, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { SpinnerLoading } from "./SpinnerLoading";
+import api from "./axios";
+
+
+export default function PersonalInfo() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [view, setView] = useState("0");
+  const [click, setClick] = useState("0");
+
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      profileId: "",
+    },
+  });
+
+  const onSubmit = async (data) => {
+    try {
+      setIsLoading(true);
+
+      const response = await api.post("/count", {
+        profileId: data.profileId,
+      });
+
+      const { success, message } = response.data;
+
+      if (!success) {
+        toast.error(message || "Please try again.");
+        return;
+      }
+
+     setView(response.data.views);
+     setClick(response.data.textMeClicks);
+
+    toast.success(message);
+
+    } catch (err) {
+      toast.error(
+        err.response?.data?.message ||
+          "Something went wrong. Please try again.",
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        minHeight: "100vh",
+        bgcolor: "#F5F7FB",
+        py: 6,
+      }}
+    >
+      <Container maxWidth="lg">
+        <Stack spacing={5}>
+          {/* Progress */}
+
+          <Box>
+            <Stack direction="row" justifyContent="space-between" mb={2}>
+              <Typography fontWeight={700} color="primary">
+                {view} - views
+              </Typography>
+
+              <Typography fontWeight={700} color="primary">
+                {click} - clicks
+              </Typography>
+            </Stack>
+          </Box>
+
+          {/* Header */}
+        </Stack>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Stack spacing={4}>
+            <Controller
+              name="profileId"
+              control={control}
+              rules={{
+                required: "user name is requiredd",
+              }}
+              render={({ field, fieldState }) => (
+                <TextField
+                  {...field}
+                  type="string"
+                  label="username"
+                  placeholder="Enter user Name"
+                  fullWidth
+                  error={!!fieldState.error}
+                  helperText={fieldState.error?.message}
+                />
+              )}
+            />
+
+            {/* Continue */}
+            <Button type="submit" size="large" variant="contained" fullWidth>
+              COUNT
+            </Button>
+          </Stack>
+        </form>
+        {isLoading && <SpinnerLoading />}
+      </Container>
+    </Box>
+  );
+}
